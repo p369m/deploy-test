d3baa85446299914cb5f05febd37e91ce8e161ba
Partha Mandal <parthamandal621@gmail.com>
2024-07-31 22:32:33 +0530

    fa

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express();
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
-
+app.get("/test", (req, res) => {
+  res.send("TEst Succesfull");
+});
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port", process.env.PORT);
 });
